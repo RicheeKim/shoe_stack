ef9635fd8bccafd4e0e190e9fb922c709b1c544c
rkim91 <richeekim@gmail.com>
2018-05-29 14:57:23 -0400

    skateboard up to login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_192522) do
+ActiveRecord::Schema.define(version: 2018_05_29_175302) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 2018_05_26_192522) do
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
