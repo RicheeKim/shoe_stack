0a2dceaefb90debb6ae5d9f6806a221eed419a99
deepgpatel <deepgpatel93@gmail.com>
2018-05-29 15:03:26 -0400

    random error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params['username'])
 
-    # user exists
     if @user.exist?
         session[:user_id] = @user.id
         redirect_to snacks_path
-    # user doesn't
     else
         flash[:message] = "User could not be found"
         render :new
