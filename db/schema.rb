d334892c3ec7f774fa7fadd0409704724e08e60c
deepgpatel <deepgpatel93@gmail.com>
2018-05-30 11:18:02 -0400

    associations of User_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_211839) do
+ActiveRecord::Schema.define(version: 2018_05_30_151555) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_211839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "brand_id"
+    t.integer "user_id"
   end
 
   create_table "sizes", force: :cascade do |t|
